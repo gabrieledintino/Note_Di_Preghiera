ce200290c43e5c2800e9cad60a062ef472f8912d
Gabriele D'Intino <gabriele.dintino@icloud.com>
2020-11-25 23:10:38 +0100

    Version 0.7.5 - Cleaned Settings interface and added contact options (TO BE FINISHED)

---
+++ Canta e Cammina/View/Tab Views/SettingsView.swift
@@ -0,0 +1,55 @@
+//
+//  SettingsView.swift
+//  Canta e Cammina
+//
+//  Created by Gabriele on 14/10/2020.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Contatti")) {
+                    NavigationLink(destination: Link("Mail", destination: URL(string: "mailto:gabriele.dintino@icloud.com")!)) {
+                        Link("Mail", destination: URL(string: "mailto:gabriele.dintino@icloud.com")!)
+                            .buttonStyle(PlainButtonStyle())
+                    }
+                    NavigationLink(destination: Link("Instagra,", destination: URL(string: "https://www.apple.com")!)) {
+                        Link("Instagram", destination: URL(string: "https://www.apple.com")!)
+                            .buttonStyle(PlainButtonStyle())
+                    }
+                    Text("Per ogni problema, suggerimento, segnalazione di bug/crash e per proporre altri canti contattami utilizzando i social qui sopra!")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                }
+                Section(header: Text("Ringraziamenti")) {
+                    NavigationLink(destination: Text("Prova")) {
+                        Text("Ringraziamenti")
+                    }
+                }
+                
+                VStack {
+                    Text("Per mia moglie, che ha ispirato questa App.")
+                    HStack {
+                        Text("Ti amo")
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.red)
+                    }
+                }
+            }
+            
+            .navigationTitle("Impostazioni")
+//            .listStyle(InsetGroupedListStyle())
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
